145ff60e652f6c1c707ab0e1a89ec41101ae5148
ssrlive <30760636+ssrlive@users.noreply.github.com>
2023-04-22 19:39:02 +0800

    info

--- platform/ios/greetings/ViewController.swift
+++ platform/ios/greetings/ViewController.swift
@@ -18,7 +18,7 @@ class ViewController: UIViewController {
         let label = UILabel(frame: CGRect(x: 0, y: 0, width: 300, height: 21))
         label.center = CGPoint(x: 160, y: 285)
         label.textAlignment = .center
-        label.text = rustGreetings.sayHello(to: "I'm here")
+        label.text = rustGreetings.sayHello(to: "from iOS")
         self.view.addSubview(label)
     }
 }
