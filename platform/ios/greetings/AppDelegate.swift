c226996e84fb6935fae96ebcd3d6aacbb860685b
ssrlive <30760636+ssrlive@users.noreply.github.com>
2023-04-22 19:03:41 +0800

    Build automation in Xcode with Rust for iOS

--- platform/ios/greetings/AppDelegate.swift
+++ platform/ios/greetings/AppDelegate.swift
@@ -10,21 +10,39 @@ import UIKit
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
-
+    var window: UIWindow?
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        if #available(iOS 13.0, *) {
+            // Code for iOS 13 and later
+        } else {
+            // Code for iOS 12 and earlier
+
+            // Create a UIWindow manually
+            window = UIWindow(frame: UIScreen.main.bounds)
+
+            // Set the root view controller
+            let viewController = ViewController()
+            viewController.view.backgroundColor = .white
+            window?.rootViewController = viewController
+
+            // Make the window visible
+            window?.makeKeyAndVisible()
+        }
         return true
     }
 
     // MARK: UISceneSession Lifecycle
 
+    @available(iOS 13.0, *)
     func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
         // Called when a new scene session is being created.
         // Use this method to select a configuration to create the new scene with.
         return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
     }
 
+    @available(iOS 13.0, *)
     func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
         // Called when the user discards a scene session.
         // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
--- src/greetings.h
+++
@@ -1,9 +0,0 @@
-#if !defined(__RUST_GREETING_H__)
-#define __RUST_GREETING_H__
-
-#include <stdint.h>
-
-const char* rust_greeting(const char* to);
-void rust_greeting_free(char *);
-
-#endif // __RUST_GREETING_H__
